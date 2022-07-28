c62d4f2c53a0dd15c82dba6fcb8860be69cf3377
ezenielrios <ezeniel.rios.er@gmail.com>
2022-07-28 17:50:30 -0400

    import cleanup beginning defect stepsimpl

---
+++ BugCatcher/src/test/java/DefectFeature.java
@@ -0,0 +1,2 @@
+public class DefectFeature {
+}
--- BugCatcher/src/test/java/LoginFeature.java
+++ BugCatcher/src/test/java/LoginFeature.java
@@ -3,14 +3,11 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 
 import java.time.Duration;
 
