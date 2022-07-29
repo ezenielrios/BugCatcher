e73a29642a649b64ba79de94dd3063a52bbcf7b5
ezenielrios <ezeniel.rios.er@gmail.com>
2022-07-29 12:10:03 -0400

    step phrases pending code impl

--- BugCatcher/src/test/java/DefectFeature.java
+++ BugCatcher/src/test/java/DefectFeature.java
@@ -1,2 +1,57 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class DefectFeature {
+
+    @Given("The manager is logged in as a manager")
+    public void the_manager_is_logged_in_as_a_manager() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Given("The manager is on the home page")
+    public void the_manager_is_on_the_home_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("The manager should see pending defects")
+    public void the_manager_should_see_pending_defects() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("The manager clicks on the select button for a defect")
+    public void the_manager_clicks_on_the_select_button_for_a_defect() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("The defect description should appear in bold")
+    public void the_defect_description_should_appear_in_bold() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("The manager selects an tester from the drop down")
+    public void the_manager_selects_an_tester_from_the_drop_down() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("The manager clicks assign")
+    public void the_manager_clicks_assign() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("The defect should disappear from the list")
+    public void the_defect_should_disappear_from_the_list() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Given("The assigned tester is on their home page")
+    public void the_assigned_tester_is_on_their_home_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("The tester should see the pending defect")
+    public void the_tester_should_see_the_pending_defect() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 }
